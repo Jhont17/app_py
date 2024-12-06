b0f89196eb5f59183a33b37f031a287fbd59a2de
sebastian <jhon-3.17@outlook.com>
2024-12-05 22:56:06 -0500

    actualizacion

--- models/estudiante_model.py
+++ models/estudiante_model.py
@@ -6,12 +6,12 @@ class Estudiante(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     correo = db.Column(db.String(100), unique=True, nullable=False)
-    #curso_id = db.Column(db.Integer, db.ForeignKey('cursos.id'), nullable=True)
+    curso_id = db.Column(db.Integer, nullable=True)
 
     def to_dict(self):
         return {
             'id': self.id,
             'nombre': self.nombre,
             'correo': self.correo,
-            #'curso_id': self.curso_id
+            'curso_id': self.curso_id
         }
